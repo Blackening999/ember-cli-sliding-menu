ca349e038f16fe087849610e15965feacfe2a0c6
Blackening999 <pointhomefinal@gmail.com>
2015-05-31 21:09:55 +0400

    fix g

--- blueprints/ember-cli-sliding-menu/index.js
+++ blueprints/ember-cli-sliding-menu/index.js
@@ -1,14 +1,13 @@
 module.exports = {
   description: '',
 
-  // locals: function(options) {
-  //   // Return custom template variables here.
-  //   return {
-  //     foo: options.entity.options.foo
-  //   };
-  // }
+  normalizeEntityName: function() {
+    // this prevents an error when the entityName is
+    // not specified (since that doesn't actually matter
+    // to us
+  },
 
   afterInstall: function(options) {
-    return this.addBowerPackageToProject('hammer');
+    return this.addBowerPackageToProject('hammerjs');
   }
 };
--- index.js
+++ index.js
@@ -5,8 +5,6 @@ module.exports = {
   name: 'ember-cli-sliding-menu',
   included: function(app) {
     this._super.included(app);
-
-    app.import('bower_components/hammerjs/hammer.js');
-    //this.app.import(app.bowerDirectory + '/remarkable/dist/remarkable.js');
+    app.import(app.bowerDirectory + '/hammerjs/hammer.js');
   }
 };
